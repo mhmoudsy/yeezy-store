c0f42f596ef05bb8968205d89e9f0a48fd26623b
mhmoudsy <mhmoudsyd27@gmail.com>
2024-01-27 01:09:25 +0200

    image

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -24,6 +24,7 @@ class RegisterController extends Controller
         $user['password'] = Hash::make($user['password']);
       
          if($request->image==null){
+            //..
             $user['image']="https://upload.najd-products.com/public/storage/images/profile-image.jpg";
         }
 
